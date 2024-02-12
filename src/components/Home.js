35b42e234d22ce5150cfb7655a40f6106017c6b6
Gakuya <sgakuya@middlebury.edu@w7pn7rmkjq.middlebury.edu>
2024-02-12 10:49:16 -0500

    Office Hours update Spring 24

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,11 +23,11 @@ export function Main({user}){
                 <div className='flex-child2' >
                     <br></br>
                     <p> <strong>Office:</strong> 75 Shannon Street 221 </p>
-                    <p><strong>Office Hours:</strong> Monday - Friday = 1:30PM - 4:30PM and by appointment</p>
+                    <p><strong>Office Hours:</strong> M 12:00-3:00PM, T 9:00AM-12:00PM, W 12:00-3:00PM, TR 9:00AM-12:00PM, F 1:00-4:00PM or by appointment</p>
                     <p><strong>Email:</strong> sgakuya@middlebury.edu </p>
-                    <p>I can comfortably offer help in 110, 145, 150, 200, 201, 202, and 312. I can also work together with you to solve problems in 301, 302, 311, and 313. 
+                    <p>I can comfortably offer help in 145, 200, 201, 202, 302 and 312. I can also work together with you to solve problems in 301 and 390. 
                         It might be helpful to send me an email with the topics/questions you are struggling with so I can help you more efficiently.<br></br><br></br>
-                        You probably know more about these topics than I do: 315, 333, 461</p>
+                        You probably know more about these topics than I do: 321, 414, 451, 452, 457</p>
                     <br></br>
                     {/* <h3><strong>IMPORTANT UPDATE: Office Hours will temporarily be over Zoom</strong></h3>
                     <p><strong>Zoom Link: </strong> <a className='zoom' href='https://middlebury.zoom.us/j/96998040243?pwd=SDdVZ2VVK3o3WUQxZjBCNWhpTytOdz09'>link</a></p>
